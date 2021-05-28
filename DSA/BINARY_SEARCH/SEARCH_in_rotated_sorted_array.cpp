d4d1e7e3b1155cd54d20bc032abe3c211433d3cc
soumya <61834029+soumy47@users.noreply.github.com>
2021-05-28 20:48:18 +0530

    Create SEARCH_in_rotated_sorted_array.cpp

---
+++ DSA/BINARY_SEARCH/SEARCH_in_rotated_sorted_array.cpp
@@ -0,0 +1,35 @@
+ int search(vector<int>& nums, int target) {
+        int high = nums.size()-1;
+        int low = 0;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[0] <= nums[mid]){
+                if(nums[0] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+    int main(){
+      vector<int> v = {4,5,6,7,0,1,2};
+      cout<<search(v, 3);
+    
+    return 0;
+    }
