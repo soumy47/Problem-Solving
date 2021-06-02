e440b9aa6ae898b1e195f53e9e9b308ad04804aa
soumya <61834029+soumy47@users.noreply.github.com>
2021-06-02 23:37:59 +0530

    Create #716a(div 2).cpp

---
+++ CP/#716a(div 2).cpp
@@ -0,0 +1,58 @@
+    #include<iostream>
+    #include<vector>
+    #include<algorithm>
+    #include<cmath>
+    using namespace std;
+    typedef long long ll;
+     
+    const ll N = 1e9+7;
+    vector<vector<ll>> a(1000, vector<ll>(1000));
+     
+    bool isPerfect(ll n){
+        int m = int(sqrt(n));
+        return ((m*m) == n);
+    }
+    ll power(ll n, ll k){
+        n %= N;
+        ll ans = 1;
+        while(k > 0){
+            if(k & 1){
+                ans = (ans * n) % N;
+            }
+            n = (n * n) % N;
+            k/=2;
+        }
+        return ans;
+    }
+    int main()
+    {
+        ios_base::sync_with_stdio(false);              //for fast input output 
+        cin.tie(NULL);                                //it help to run code fast 
+        int t;
+        cin>>t;
+        while(t--)
+        {  
+            ll n;
+            cin>>n;
+            ll a[n];
+            for(int i=0;i<n;i++){
+                cin>>a[i];
+            }
+            for(int i=0;i<n;i++){
+                if(a[i] &1)
+                {
+                    cout<<a[i]<<" ";
+                }
+            }
+             for(int i=0;i<n;i++){
+                if((a[i] &1) == 0)
+                {
+                    cout<<a[i]<<" ";
+                }
+            }
+     
+     
+          cout<<"\n";
+        }
+        return 0;
+    }
