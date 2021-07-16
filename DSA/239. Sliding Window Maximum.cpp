71cfae151b217f58281cec196035504b94c0a22a
soumya <61834029+soumy47@users.noreply.github.com>
2021-07-16 21:33:33 +0530

    Create 239. Sliding Window Maximum.cpp

---
+++ DSA/239. Sliding Window Maximum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+        
+        int n = nums.size();
+       // dq.push_back(0);
+        for(int i = 0; i< n; i++ )
+        {
+            if(!dq.empty()   &&  dq.front() == (i-k))  // boundry check
+                dq.pop_front();
+            
+            while(!dq.empty()   &&  nums[dq.back()] < nums[i])
+                dq.pop_back();
+            
+            dq.push_back(i);
+            
+            if(i >= k-1)
+                ans.push_back(nums[dq.front()]);
+            
+        }
+        return ans;
+    }
+};
